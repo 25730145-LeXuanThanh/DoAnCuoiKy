8a9d94044bb777c024781a174e72aadc1ebfc3f4
25730145-LeXuanThanh <25730145@ms.uit.edu.vn>
2026-05-11 22:47:35 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -76,28 +76,40 @@ char blocks[][4][4] = {
      {' ', ' ', ' ', ' '}}};
 
 // ===== COLOR =====
-string getColor(char c){
-    switch(c){
-        case 'I': return "\033[96m";
-        case 'O': return "\033[93m";
-        case 'T': return "\033[95m";
-        case 'S': return "\033[92m";
-        case 'Z': return "\033[91m";
-        case 'J': return "\033[94m";
-        case 'L': return "\033[33m";
-        default: return "\033[0m";
-    }
+string getColor(char c)
+{
+        switch (c)
+        {
+        case 'I':
+                return "\033[96m";
+        case 'O':
+                return "\033[93m";
+        case 'T':
+                return "\033[95m";
+        case 'S':
+                return "\033[92m";
+        case 'Z':
+                return "\033[91m";
+        case 'J':
+                return "\033[94m";
+        case 'L':
+                return "\033[33m";
+        default:
+                return "\033[0m";
+        }
 }
-void gotoXY(int x, int y){
-    COORD c = {(SHORT)x,(SHORT)y};
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+void gotoXY(int x, int y)
+{
+        COORD c = {(SHORT)x, (SHORT)y};
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
-void hideCursor(){
-    CONSOLE_CURSOR_INFO ci;
-    ci.bVisible = FALSE;
-    ci.dwSize = 100;
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ci);
+void hideCursor()
+{
+        CONSOLE_CURSOR_INFO ci;
+        ci.bVisible = FALSE;
+        ci.dwSize = 100;
+        SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ci);
 }
 
 bool canMove(int dx, int dy)
@@ -109,17 +121,22 @@ void block2Board()
 void boardDelBlock()
 {
 }
-void initBoard(){
-    for (int i = 0 ; i < H ; i++)
-        for (int j = 0 ; j < W ; j++)
-            if (i == 0 || i == H-1 || j ==0 || j == W-1) board[i][j] = '#';
-            else board[i][j] = ' ';
+void initBoard()
+{
+        for (int i = 0; i < H; i++)
+                for (int j = 0; j < W; j++)
+                        if (i == 0 || i == H - 1 || j == 0 || j == W - 1)
+                                board[i][j] = '#';
+                        else
+                                board[i][j] = ' ';
 }
-void draw(){
-    system("cls");
+void draw()
+{
+        system("cls");
 
-    for (int i = 0 ; i < H ; i++, cout<<endl)
-        for (int j = 0 ; j < W ; j++) cout<<board[i][j];
+        for (int i = 0; i < H; i++, cout << endl)
+                for (int j = 0; j < W; j++)
+                        cout << board[i][j];
 }
 void removeLine()
 {
@@ -127,6 +144,14 @@ void removeLine()
 
 int main()
 {
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        DWORD dwMode = 0;
+        GetConsoleMode(hOut, &dwMode);
+
+        dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+        SetConsoleMode(hOut, dwMode);
         srand(time(0));
         x = 5;
         y = 0;
@@ -163,4 +188,3 @@ int main()
         }
         return 0;
 }
-
