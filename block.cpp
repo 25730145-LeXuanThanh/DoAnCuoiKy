71a06287b6b04ac2c5b63adb1dd8d99527384436
25730129 <25730129@ms.uit.edu.vn>
2026-05-15 23:41:52 +0700

    (Tuần 3A) Đưa hàm canMove vào trong class. Tạo hàm di chuyển cho class

--- block.cpp
+++ block.cpp
@@ -18,4 +18,27 @@ public:
     Block() : x(5), y(1) {}
 
     void rotate(){};
+    
+bool canMove(int dx, int dy)
+{
+        for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                        if (blocks[b][i][j] != ' ')
+                        {
+                                int xt = x + j + dx;
+                                int yt = y + i + dy;
+                                if (xt < 1 || xt >= W - 1 || yt >= H - 1)
+                                        return false;
+                                if (board[yt][xt] != ' ')
+                                        return false;
+                        }
+        return true;
+}
+void move(int dx, int dy)
+    {
+        if(canMove(dx, dy))
+        {x += dx;
+        y += dy;
+        }
+    }
 };
